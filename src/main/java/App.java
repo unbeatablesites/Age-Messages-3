ea16ae7297065be586d28015289c6ec490e05a4e
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 00:35:12 -0500

    Done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-
+        askQuestion();
     }
 
     public static void askQuestion(){
+
+        System.out.println("Please enter your age");
 
         Scanner scan = new Scanner(System.in);
 
@@ -14,7 +16,12 @@ public class App {
 
         if(input < 16 ){
             System.out.println("You can't drive");
-        }
+        }else if (input == 16 || input == 17){
+            System.out.println("You can drive but not vote.");
+        }else if (input >= 18 && input <= 24){
+            System.out.println("You can vote but not rent a car.");
+        }else
+            System.out.println("You can do pretty much anything.");
 
     }
 
