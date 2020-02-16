0c51b7c66b67724fa84dc93e2d8fbba05a2191b9
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-16 00:24:57 -0500

    Add first condition

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void askQuestion(){
+
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        if(input < 16 ){
+            System.out.println("You can't drive");
+        }
+
+    }
+
 }
